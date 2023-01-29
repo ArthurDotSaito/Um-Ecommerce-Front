bf4c25e025142900d1f04eacd908956b4293bba4
Jefferson Araújo <59322029+jeffersonAraujo2012@users.noreply.github.com>
2023-01-29 19:13:12 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyles";
-import './assets/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
